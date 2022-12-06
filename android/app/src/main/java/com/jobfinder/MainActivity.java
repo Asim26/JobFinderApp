8012a35baa94fa91060657348cace66c12cdf831
asimmehmood.codingpixel11 <asim.mehmood@codingpixel.com>
2022-12-06 10:51:40 +0500

    splash screen added on android

--- android/app/src/main/java/com/jobfinder/MainActivity.java
+++ android/app/src/main/java/com/jobfinder/MainActivity.java
@@ -1,6 +1,8 @@
 package com.jobfinder;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
@@ -14,6 +16,12 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "JobFinder";
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
